05fffc4952d415aba2acdfffc368f7db74ae11ab
Scott Betzel <scott.betzel@gmail.com>
2019-03-01 15:23:19 -0800

    Added button to start the game

--- src/App.js
+++ src/App.js
@@ -5,23 +5,47 @@ import {default as GameModel} from "./model/Game";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        let game = new GameModel();
+    this.state = {
+      game: null,
+      gesture: false
+    };
+  }
 
-        this.state = {
-            game: game
-        };
+  _onMouseClick = () => {
+    if (this.state.gesture) {
+      return;
     }
 
-    render() {
-        return (
-            <div className="App" style={{backgroundColor: "Gray"}}>
-                <Game className="AppGame" dataSource={this.state.game} />
-            </div>
-        );
+    this.setState({
+      gesture: true,
+      game: new GameModel(),
+    });
+  };
+
+  renderContent() {
+    if (this.state.gesture && this.state.game) {
+      return (
+        <Game className="AppGame" dataSource={this.state.game} />
+      );
     }
+
+    return (
+      <div className={"WaitingForGesture"}>
+        <button onClick={this._onMouseClick} className={"begin-button"}>{"Click to Begin"}</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App" style={{backgroundColor: "black"}}>
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
 
 export default App;
